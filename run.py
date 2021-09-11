c5681a7d764153ba51044c2c9ee90190298ebfc8
Markp1312 <mpg.peeters3@gmail.com>
2021-09-11 14:46:10 +0000

    Add surplus to worksheet

--- run.py
+++ run.py
@@ -60,6 +60,15 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("surplus worksheet updated successfully.\n")
+
 def calculate_surplus_data(sales_row):
 
     """
@@ -77,7 +86,7 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock)-sales
         surplus_data.append(surplus)
-    print(surplus_data)    
+    return surplus_data()   
 
 def main():
 
@@ -88,5 +97,6 @@ data = get_sales_data()
 sales_data = [int(num) for num in data]
 update_sales_worksheet(sales_data)
 new_surplus_data = calculate_surplus_data(sales_data)
+update surplus_worksheet(new_surplus_data)
 
 main()
